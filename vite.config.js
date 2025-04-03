944b8a35837b073dc88d9270f85f904e02818014
jimc <jimc@bethel.com.tw>
2025-04-03 06:27:13 +0000

    2025-04-03 14:30

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // 使用相对路径
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+      rollupOptions: {
+          output: {
+              entryFileNames: `assets/[name].js`,
+              chunkFileNames: `assets/[name].js`,
+              assetFileNames: `assets/[name].[ext]`
+          }
+      },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8080
+  }
 })
