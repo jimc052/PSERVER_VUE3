b5878760a487ce0be88da5e533b51fc3069f40ce
jim <jimc@bethel.com.tw>
2025-04-10 18:00:41 +0800

    2025-04-10

--- src/Exercise.vue
+++ src/Exercise.vue
@@ -35,14 +35,7 @@ export default {
       let s = this.$refs.textarea1.value.trim(), result = "";
       if(s.indexOf("{") == 0) {
         let json = JSON.parse(s);
-        if(typeof json["props"] == "object") {
-          if(json["props"].beep == true) {
-            result += "BEEP#\n"
-          }
-          if(typeof json["props"].prnSize == "number") { // ;[#PrnSize:80]
-            result += `;[#PrnSize:${json["props"].prnSize}]\n`
-          }
-        }
+        result = this.assembleToFile(json);
 
       } else {
         let json = this.$parseText(this.$refs.textarea1.value.trim());
@@ -58,6 +51,99 @@ export default {
       localStorage.setItem("page", "App");
       location.reload();
     },
+    assembleToFile(json) {
+      let result = "", section;
+      let retrieve = (arr) => {
+        let arr1 = JSON.parse(JSON.stringify(arr)), arr2 = [];
+        for(let i = 0; i < arr1.length; i++) {
+          if(i == 0 || arr1[i].top != arr1[i - 1].top ) {
+            
+            if(i > 0) {
+              let top1 = arr1[i - 1].top, top2 = arr1[i].top;
+              while (top1 < top2) { // 空白列
+                arr2.push([]);
+                top1 += this.$cellHeight;
+              }
+            }
+            arr2.push([])
+          }
+          arr2[arr2.length - 1].push(arr1[i])
+        }
+        arr2.forEach(el1 => {
+          el1.forEach(el2 => {
+            delete el2.top;
+            delete el2.left;
+            if(typeof el2.props == "object") {
+              if(el2.props.sz == "1") delete el2.props.size;
+              if(el2.props.al == "0") delete el2.props.al;
+              if(Object.keys(el2.props).length == 0) delete el2.props;
+            }
+          })
+        })
+        return arr2;
+      }
+      if(typeof json["props"] == "object") {
+        if(json["props"].beep == true) {
+          result += "BEEP#\n"
+        }
+        if(typeof json["props"].prnSize == "number") { // ;[#PrnSize:80]
+          result += `;[#PrnSize:${json["props"].prnSize}]\n`
+        }
+      }
+
+      let sections = ["header", "detail", "footer1", "payment", "footer2"];
+      for(let i = 0; i < sections.length; i++) {
+        section = sections[i]; 
+        let sectionResult = "", flag;
+        if(section == "header") {
+          flag = "H#";
+        } else if(section == "detail") {
+          flag = "I#";
+        } else if(section == "footer1") {
+          flag = "F#";
+        }
+        // if(section != "header") continue;
+        // console.log(section)
+        let arr = [];
+        if(section == "detail"){
+          if(Array.isArray(json[section].items) && json[section].items.length > 0) { 
+            arr = retrieve(json[section].items)
+          }
+        } else if(Array.isArray(json[section]) && json[section].length > 0) { 
+          arr = retrieve(json[section])
+        }
+        for(let j = 0; j < arr.length; j++) {
+          let line = "";
+          for(let k = 0; k < arr[j].length; k++) {
+            let row = arr[j][k], title = row.title;
+            // console.log(JSON.stringify(row))
+            if(title == "自定文字") {
+              title = row.props.text;
+              delete row.props.text;
+              if(Object.keys(row.props).length == 0) {
+                line += title;
+                row = undefined
+              }
+            }
+            if(typeof row == "object") {
+              let props = typeof row.props == "object" && Object.keys(row.props).length > 0 ? ":" : "";
+              for(let key in row.props) {
+                props += (props.length > 1 ? ";" : "") + key + "=" + row.props[key];
+              }
+              line += `[${title}${props}]`
+            }
+          }
+          sectionResult += line + "\n";
+        }
+        if(sectionResult.length > 0) {
+          result += (typeof flag == "string" ? (flag + "\n") : "") + sectionResult;
+        }
+      }
+      console.log(result)
+
+
+      return result;
+    }
   },
   computed: {
   },
